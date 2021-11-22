2ef09a37e6a3b979cd58aebcf1d4dc65c772d93c
денис эм <denisem@deniss-MacBook-Pro.local>
2021-11-22 18:07:42 +0300

    codestyle

--- src/main/java/com/example/springbootbootstrap/dao/UserRepository.java
+++ src/main/java/com/example/springbootbootstrap/dao/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-//    public User findByName(String name);
 
     @Query("SELECT u FROM User u JOIN FETCH u.roleList WHERE u.email = (:email)")
     public User findByName(@Param("email") String email);
